259493bdefddeb90bb1427d82257bff6cfd2bd59
Peter Alabaster <peter.alabaster@opusvl.com>
2016-04-08 17:06:58 +0100

    Moving unicodewriter to new file

--- odoo_batch_export_base/models/base_batch_export.py
+++ odoo_batch_export_base/models/base_batch_export.py
@@ -21,7 +21,9 @@
 ##############################################################################
 
 from openerp import models, fields, api, exceptions
-import time, tempfile, csv, codecs, cStringIO, shutil, gzip, os
+from unicode_writer import UnicodeWriter
+import time, shutil, gzip, os
+# import tempfile
 
 # TODO Write to tempfile before end-product
 
@@ -66,33 +68,5 @@ class BatchExport(models.Model):
                 shutil.copyfileobj(f_in, f_out)
             os.remove(filename)
 
-class UnicodeWriter:
-    """
-    A CSV writer which will write rows to CSV file "f",
-    which is encoded in the given encoding.
-    """
-
-    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
-        # Redirect output to a queue
-        self.queue = cStringIO.StringIO()
-        self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
-        self.stream = f
-        self.encoder = codecs.getincrementalencoder(encoding)()
-
-    def writerow(self, row):
-        self.writer.writerow([s.encode("utf-8") for s in row])
-        # Fetch UTF-8 output from the queue ...
-        data = self.queue.getvalue()
-        data = data.decode("utf-8")
-        # ... and reencode it into the target encoding
-        data = self.encoder.encode(data)
-        # write to the target stream
-        self.stream.write(data)
-        # empty queue
-        self.queue.truncate(0)
-
-    def writerows(self, rows):
-        for row in rows:
-            self.writerow(row)
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ odoo_batch_export_base/models/unicode_writer.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+##############################################################################
+#
+# odoo_batch_export_base
+# Copyright (C) 2016 OpusVL (<http://opusvl.com/>)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+import codecs, cStringIO, csv
+
+
+class UnicodeWriter:
+    """
+    A CSV writer which will write rows to CSV file "f",
+    which is encoded in the given encoding.
+    """
+
+    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
+        # Redirect output to a queue
+        self.queue = cStringIO.StringIO()
+        self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
+        self.stream = f
+        self.encoder = codecs.getincrementalencoder(encoding)()
+
+    def writerow(self, row):
+        self.writer.writerow([s.encode("utf-8") for s in row])
+        # Fetch UTF-8 output from the queue ...
+        data = self.queue.getvalue()
+        data = data.decode("utf-8")
+        # ... and reencode it into the target encoding
+        data = self.encoder.encode(data)
+        # write to the target stream
+        self.stream.write(data)
+        # empty queue
+        self.queue.truncate(0)
+
+    def writerows(self, rows):
+        for row in rows:
+            self.writerow(row)
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
